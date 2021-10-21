e6d2b76092dc41b81aebf418ec1c7e3ed54c26e6
Jeongwon (Miley) Seo <60459873+mileyseo@users.noreply.github.com>
2021-10-21 18:52:36 -0500

    Recreate Home Page
    to separate home from inventory

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import Footer from '../components/Footer'
-import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <h2>Inventory Hub</h2>
-      <hr></hr>
-      <div className="dropdown m-3 dropend">
-        <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" id="dropdownMenuButton1" aria-expanded="false">
-          Select Category {" "}
-        </button>
-        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-          <li><a className="dropdown-item" href="#"> Item 1</a></li>
-          <li><a className="dropdown-item" href="#"> Item 2</a></li>
-          <li><a className="dropdown-item" href="#"> Item 3</a></li>
-        </ul>
-      </div>
+    <div>
+      <h1 className={styles.title}> Welcome to H-E-B! </h1>
+      <Link href="/inventory"><button type="button" className={styles.btn}> Check Inventory </button></Link>
     </div>
   )
 }
