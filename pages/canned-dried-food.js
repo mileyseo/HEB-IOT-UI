9c42dfbfa7c809c233d5dba83f3c43ac31a87330
Jeongwon (Miley) Seo <60459873+mileyseo@users.noreply.github.com>
2021-10-22 16:01:19 -0500

    Pantry Page
    Added Categories

---
+++ pages/canned-dried-food.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Home.module.css'
+import Link from 'next/link'
+
+export default function CannedDriedFood() {
+  return (
+    <div>
+      <h1 className={styles.title}> Canned {"&"} Dried Food </h1>
+    </div>
+  )
+}
