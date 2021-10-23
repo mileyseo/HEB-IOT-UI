ff0980d0b3df1e63c4dd926463f8c6085dc9507f
Nicole Rodrigues <nicolerodrigues@Nicoles-MacBook-Pro.local>
2021-10-22 19:08:04 -0500

    basic contact page done

--- pages/contact.js
+++ pages/contact.js
@@ -2,7 +2,14 @@ import styles from '../styles/Home.module.css'
 
 export default function Contact() {
     return (
-        <h2 className={styles.container}>Contact Us</h2>
-
+        <div>
+            <h2 className={styles.container}>Contact Us</h2>
+            <div className="contact">
+                <h3> Comments and compliments — we want to hear them all. Let us know how we're doing so we can serve you better. </h3>
+                <br/>
+                <h3> Contact us at  <a href="mailto: abc@example.com"> email@heb.com </a></h3>
+                <h3> For more resources regarding HEB, head to the <a href="https://www.heb.com/static-pages/faq?page=Help-FAQ" target="_blank">Help Center.</a></h3>
+            </div>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,6 @@ export default function Home() {
     <div>
       <h1 className={styles.title}> Welcome to H-E-B! </h1>
       <Link href="/inventory"><button type="button" className={styles.btn}> Check Inventory </button></Link>
-        <Link href="/pantry"><button type="button" className={styles.btn}> Aisle Page </button></Link>
-
     </div>
   )
 }
