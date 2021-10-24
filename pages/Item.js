a8972b432c4d3f5009566faa97f0351a60da5da4
Nicole Rodrigues <nicolerodrigues@Nicoles-MacBook-Pro.local>
2021-10-24 13:43:20 -0500

    updating item page

--- pages/Item.js
+++ pages/Item.js
@@ -7,37 +7,6 @@ class Item{
         this._qty = qty;
     }
 
-    let items = [
-        {
-            "name" : "H‑E‑B Select Ingredients Crisp N' Sweet Whole Kernel Corn";
-            "id": 1;
-            "aisle" : 6;
-            "qty" : 100;
-
-        },
-        {
-            "name" : "H‑E‑B Select Ingredients Cut Green Beans";
-            "id": 2;
-            "aisle" : 6;
-            "qty" : 123;
-
-        },
-        {
-            "name" : "H‑E‑B Organics Diced Tomatoes";
-            "id": 3;
-            "aisle" : 6;
-            "qty" : 234;
-
-        },
-        {
-            "name" : "H‑E‑B Select Ingredients Whole Kernel Corn";
-            "id": 4;
-            "aisle" : 6;
-            "qty" : 234;
-
-        },
-    ]
-
     get name() {
         return this._name;
     }
