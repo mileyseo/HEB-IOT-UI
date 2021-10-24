02b5360e91db7e2b36ab5368cd786cf4deefab81
Nicole Rodrigues <nicolerodrigues@Nicoles-MacBook-Pro.local>
2021-10-24 14:17:04 -0500

    fixing json data format

--- pages/canned-dried-food.js
+++ pages/canned-dried-food.js
@@ -1,7 +1,21 @@
 import styles from '../styles/Home.module.css'
+import { InferGetStaticPropsType } from 'next'
+import Item from '../pages/index.js'
 import Link from 'next/link'
 
-export default function CannedDriedFood() {
+
+export const getStaticProps = async () => {
+    const res = await fetch('https://.../posts')
+    const posts: Item[] = await res.json()
+
+    return {
+        props: {
+            posts,
+        },
+    }
+}
+
+export default function CannedDriedFood({ posts }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div>
       <h1 className={styles.title}> Canned {"&"} Dried Food </h1>
