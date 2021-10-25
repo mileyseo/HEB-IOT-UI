a5b1bfebf11d8c5b001a3da105eb78aaefe1b370
Jeongwon (Miley) Seo <60459873+mileyseo@users.noreply.github.com>
2021-10-24 23:34:05 -0500

    List of Items

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 export const itemList = [
  {
   "id": 1,
-  "name": "Sweet Corn",
+  "itemNo": 13291,
+  "name": "Libby's Whole Kernel Sweet Corn, 15.25 oz",
   "aisle": "6L",
   "section": 3,
   "shelf": 4,
@@ -9,15 +10,17 @@ export const itemList = [
   },
   {
     "id": 2,
-    "name": "Super Sweet Corn",
+    "itemNo": 32919,
+    "name": "H-E-B Select Ingredients Cut Green Beans, 14.5 oz",
     "aisle": "6L",
-    "section": 3,
-    "shelf": 3,
+    "section": 1,
+    "shelf": 2,
     "qty": 70,
   },
   {
     "id": 3,
-    "name": "Chili",
+    "itemNo": 41828,
+    "name": "Hunt's Tomato Sauce, 8 oz",
     "aisle": "6R",
     "section": 2,
     "shelf": 1,
--- pages/canned-dried-food.js
+++ pages/canned-dried-food.js
@@ -1,20 +1,48 @@
 import {itemList} from "../data"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 
 const CannedDriedFood = () => {
   return (
-      <div>
-          <h1> Canned and Dried Foods </h1>
+    <div>
+      <h1> Canned and Dried Foods </h1>
+      <div className="input-group rounded">
+      <input type="search" className="form-control rounded" placeholder="Search" aria-label="Search"
+        aria-describedby="search-addon" />
+      <span className="input-group-text border-0" id="search-addon">
+      <i><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></i>
+      </span>
+    </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Item #</th>
+            <th scope="col">Item Name</th>
+            <th scope="col">Aisle #</th>
+            <th scope="col">Section #</th>
+            <th scope="col">Shelf #</th>
+            <th scope="col">Quantity</th>
+          </tr>
+        </thead>
+        <tbody>
           {itemList.map((item) => (
-              <div key={item.id}>
-                  <a>
-                      <h3> { item.name } </h3>
-                      <h3> {item.section}</h3>
-                  </a>
-              </div>
-          ))}
-      </div>
+            <tr key={item.id}>
+              <td>{item.itemNo}</td>
+              <td>{item.name}</td>
+              <td>{item.aisle}</td>
+              <td>{item.section}</td>
+              <td>{item.shelf}</td>
+              <td>{item.qty}</td>
+            </tr>
+          ))}              
+        </tbody>
+      </table>
+    </div>
   )
 };
 
 export default CannedDriedFood;
 
+//Note for future:
+//Need to add a conditional statement to make the <50 red, and others green
+//Search Bar!
